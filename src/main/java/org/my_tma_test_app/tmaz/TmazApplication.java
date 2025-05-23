59750d02960605f23cbbe4a033b83e121c727e38
Alex <alex@Alexs-Laptop.local>
2025-05-23 13:38:50 +0300

    init commit

---
+++ src/main/java/org/my_tma_test_app/tmaz/TmazApplication.java
@@ -0,0 +1,13 @@
+package org.my_tma_test_app.tmaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmazApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/my_tma_test_app/tmaz/TmazApplicationTests.java
@@ -0,0 +1,13 @@
+package org.my_tma_test_app.tmaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
